505ce15ad4ef9505944efa2571e78785e056b184
simrithshan <83267294+simrithshan@users.noreply.github.com>
2021-05-06 00:19:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "image_id" {
+   type = string
+   default = "ami-09e67e426f25ce0d7"
+}
+
+variable "vpc_value" {
+   type = string
+   default = "my vpc"
+}
+
+variable "ecr_value" {
+   type = string
+   default = "bar"
+}
